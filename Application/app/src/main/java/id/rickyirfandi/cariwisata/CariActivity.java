27234848c336a61a6cba6ac6115642c1dc748539
Ricky Irfandi <ricky.irfandi2@gmail.com>
2018-12-01 00:06:31 +0700

    extract JSON into object
    temen temenku udah banyak yg selesai aplikasinya, dan aku masih stuck disini :'

--- Application/app/src/main/java/id/rickyirfandi/cariwisata/CariActivity.java
+++ Application/app/src/main/java/id/rickyirfandi/cariwisata/CariActivity.java
@@ -5,20 +5,21 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.os.Looper;
-import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 import android.widget.Toast;
 
-import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.location.LocationSettingsRequest;
 import com.google.android.gms.location.SettingsClient;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import static com.google.android.gms.location.LocationServices.getFusedLocationProviderClient;
 
@@ -39,10 +40,34 @@ public class CariActivity extends AppCompatActivity {
 
         status = findViewById(R.id.textView4);
         startLocationUpdates();
+        BuatListWisata();
+
     }
 
-    protected void startLocationUpdates() {
+    private void BuatListWisata(){
+        String JSON = Data.JSON_Data;
+        try{
+            JSONArray wisata = new JSONArray(JSON);
 
+            String test = "";
+            for (int i = 0; i < wisata.length(); i++) {
+                JSONObject c = wisata.getJSONObject(i);
+                String id = c.getString("id");
+                String nama = c.getString("nama");
+                String alamat = c.getString("alamat");
+                test += "\n id/nama/alamat\n" + id + nama + alamat;
+            }
+
+            status.setText(test);
+
+        } catch(Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+
+    protected void startLocationUpdates() {
         // Create the location request to start receiving updates
         mLocationRequest = new LocationRequest();
         mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
@@ -69,24 +94,24 @@ public class CariActivity extends AppCompatActivity {
                         @Override
                         public void onLocationResult(LocationResult locationResult) {
                             // do work here
-                            status.setText("onLocationResult");
-                            onLocationChanged(locationResult.getLastLocation());
+                            Location location = locationResult.getLastLocation();
+                            Data.latitude = location.getLatitude();
+                            Data.longitude = location.getLongitude();
                         }
                     },
                     Looper.myLooper());
         }
         }
 
-    public void onLocationChanged(Location location) {
-        // New location has now been determined
-        String msg = "Updated Location: " +
-                Double.toString(location.getLatitude()) + "," +
-                Double.toString(location.getLongitude());
-        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
-        status.setText(msg);
-        Data.latitude = location.getLatitude();
-        Data.longitude = location.getLongitude();
-        // You can now create a LatLng Object for use with maps
-        //LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+
+
+    //Haversine Formula
+    public double Jarak(double lat1, double lon1, double lat2, double lon2){
+        int RADIUS_BUMI = 6371;
+        double lat1Rad = Math.toRadians(lat1);
+        double lat2Rad = Math.toRadians(lat2);
+        double deltaLonRad = Math.toRadians(lon2 - lon1);
+        double hasil = Math.acos(Math.sin(lat1Rad) * Math.sin(lat2Rad) + Math.cos(lat1Rad) * Math.cos(lat2Rad) * Math.cos(deltaLonRad)) * RADIUS_BUMI;
+        return (hasil*1000); // in meters
     }
 }
--- Application/app/src/main/java/id/rickyirfandi/cariwisata/MenuActivity.java
+++ Application/app/src/main/java/id/rickyirfandi/cariwisata/MenuActivity.java
@@ -5,11 +5,13 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MenuActivity extends AppCompatActivity {
 
 ImageButton btn_cari, btn_about;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
